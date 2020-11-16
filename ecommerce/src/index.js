999e785ad5de71a1141b9ab336b0d12c0745fc7b
chamano sidney <chamano.sr@gmail.com>
2020-11-16 09:19:19 +0200

    react route

--- ecommerce/src/index.js
+++ ecommerce/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter as Router} from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <App></App>
+  </Router>,
   document.getElementById('root')
 );
 
