4a3c16303c405471fb86c82b733a1ffb0c87dfce
chamano sidney <chamano.sr@gmail.com>
2020-11-18 12:19:23 +0200

    title component

--- ecommerce/src/Components/Product.js
+++ ecommerce/src/Components/Product.js
@@ -4,7 +4,7 @@ export default class Product extends Component {
     render() {
         return (
             <div>
-                <h3>Shalom from Product</h3>
+                <h3>Component Product</h3>
             </div>
         )
     }
--- ecommerce/src/Components/ProductList.js
+++ ecommerce/src/Components/ProductList.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
 import Product from './Product'
+import Title from './Title'
 
 export default class ProductList extends Component {
+    state={
+        product:[]
+    }
     render() {
         return (
-            <div>
-               <Product></Product>
-            </div>
+        <React.Fragment>
+                <div className="py-5">
+                    <div className="container">
+                        <Title name="our" title="products"></Title>
+                        <div class  Name="row"></div>
+                    </div>
+                </div>
+
+
+        </React.Fragment>
         )
     }
 }
