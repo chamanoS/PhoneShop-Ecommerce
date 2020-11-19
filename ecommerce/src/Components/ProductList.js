4f453b2f215f029893c1435cf878f4a2d8fb45aa
chamano sidney <chamano.sr@gmail.com>
2020-11-19 08:51:01 +0200

    displaying products

--- ecommerce/src/Components/ProductList.js
+++ ecommerce/src/Components/ProductList.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react'
 import Product from './Product'
 import Title from './Title'
-import {storeProducts} from '../data'
 import { ProductConsumer } from '../Context'
 
 export default class ProductList extends Component {
-    state={
-        products: storeProducts
-    }
     render() {
-        console.log(this.state.products)
         return (
         <React.Fragment>
                 <div className="py-5">
