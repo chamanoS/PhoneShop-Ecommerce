4a9492bb22f65d3e55ceec7d5a22c1dd406bafba
chamano sidney <chamano.sr@gmail.com>
2020-11-19 12:17:47 +0200

    prod details

--- ecommerce/src/Context.js
+++ ecommerce/src/Context.js
@@ -7,8 +7,21 @@ const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
     state ={
-        products: storeProducts,
+        products: [],
         detailProduct:detailProduct
+    }
+    componentDidMount(){
+        this.setProducts()
+    }
+    setProducts = ()=>{
+        let tempProducts = [];
+        storeProducts.forEach(item =>{
+            const singleItem = {...item};
+            tempProducts = [...tempProducts, singleItem];
+        })
+        this.setState(()=>{
+            return{ products: tempProducts}
+        })
     }
     handleDetails = ()=>{
         console.log('hello from detail')
