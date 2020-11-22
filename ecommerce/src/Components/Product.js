2083f7bb22ecf0db1f2e4ee3b9a59f46f3671020
chamano sidney <chamano.sr@gmail.com>
2020-11-22 15:56:19 +0200

    cart functionality

--- ecommerce/src/Components/Product.js
+++ ecommerce/src/Components/Product.js
@@ -10,18 +10,21 @@ export default class Product extends Component {
         return (
             <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
                 <div className="card">
-                    <div className="img-container p-5" onClick={()=>console.log('you clicked on image')}>
-                        <Link to="/details">
-                            <img src={img} alt="product" className="card-img-top"/>
-                        </Link>
-                        <button className="card-btn" disable={inCart ? true : false}
-                         onClick={()=>{console.log('added to the cart')}}>
-                            {inCart ? (
-                                <p className="text-capitalize mb-0">{" "} in cart</p>
-                            ) : (<i className="fa fa-cart-plus"></i>
-                            )}
-                        </button>
-                    </div>
+                    <ProductConsumer>
+                        {(value) => (<div className="img-container p-5" onClick={() => value.handleDetails(id)}>
+                            <Link to="/details">
+                                <img src={img} alt="product" className="card-img-top" />
+                            </Link>
+                            <button className="card-btn" disable={inCart ? true : false}
+                                onClick={() => { value.addToCart(id) }}>
+                                {inCart ? (
+                                    <p className="text-capitalize mb-0">{" "} in cart</p>
+                                ) : (<i className="fa fa-cart-plus"></i>
+                                    )}
+                            </button>
+                        </div>)}
+                    
+                    </ProductConsumer>
                     <div className="card-footer d-flex justify-content-between">
                        <p className="align-self-center mb-0">
                            {title}
