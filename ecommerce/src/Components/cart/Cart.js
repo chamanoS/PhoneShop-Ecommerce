d3755cb4b8e092c14bc850cb0acc606a412ac2f4
chamano sidney <chamano.sr@gmail.com>
2020-11-22 19:53:04 +0200

    emptyCart

--- ecommerce/src/Components/cart/Cart.js
+++ ecommerce/src/Components/cart/Cart.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
 import Title from '../Title'
+import CartColumns from './CartColumns'
+import EmptyCart from './EmptyCart'
+import {ProductConsumer} from '../../Context'
 
 export default class Cart extends Component {
     render() {
         return (
             <section>
-                <Title name="your" title="cart"></Title>
+                <ProductConsumer>
+                    {value =>{
+                        const {cart} = value;
+                        if(cart.length>0){
+                            return(
+                                <React.Fragment>
+                                     <Title name="your" title="cart"/>
+                                     <CartColumns/>
+                                </React.Fragment>
+                            )
+                        }
+                        else{
+                         return <EmptyCart/>
+                        }
+                    }}
+                </ProductConsumer>
             </section>
         )
     }
--- ecommerce/src/Components/cart/CartColumns.js
+++ ecommerce/src/Components/cart/CartColumns.js
@@ -3,8 +3,27 @@ import React, { Component } from 'react'
 export default class CartColumns extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="container-fluid text-center d-none d-lg-block">
+                <div className="row">
+                    <div className="col-10 mx-auto col-lg-2">
+                        <p>products</p>
+                    </div>
+                    <div className="col-10 mx-auto col-lg-2">
+                        <p>name of products</p>
+                    </div>
+                    <div className="col-10 mx-auto col-lg-2">
+                        <p>price</p>
+                    </div>
+                    <div className="col-10 mx-auto col-lg-2">
+                        <p>quantity</p>
+                    </div>
+                    <div className="col-10 mx-auto col-lg-2">
+                        <p>remove</p>
+                    </div>
+                    <div className="col-10 mx-auto col-lg-2">
+                        <p>total</p>
+                    </div>
+                </div>
             </div>
         )
     }
