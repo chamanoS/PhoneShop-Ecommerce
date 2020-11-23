1054b30bfd6dff3284e3248a8afb84f24603ae75
chamano sidney <chamano.sr@gmail.com>
2020-11-23 09:21:16 +0200

    decrement functionality

--- ecommerce/src/Context.js
+++ ecommerce/src/Context.js
@@ -91,7 +91,21 @@ class ProductProvider extends Component {
     }
 
     decrement = (id)=>{
-        console.log('this is decrement method')
+        let tempCart = [...this.state.cart]
+        const selectedProduct = tempCart.find(item => item.id === id)
+        const index = tempCart.indexOf(selectedProduct);
+        const product = tempCart[index]; 
+        product.count = product.count - 1;
+
+        if(product.count === 0){
+            this.removeItem(id);
+        } else{
+            product.total = product.count * product.price;
+            this.setState(()=>{
+                return { cart:[...tempCart]}
+            },()=>{this.addTotals()})
+        }
+        
     }
 
     removeItem = (id)=>{
